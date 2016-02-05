df0df0b7392234e2140f27c1d35e851c2d14a35e
Mark Tompkins <mtompkins@users.noreply.github.com>
2016-02-05 09:33:50 -0500

    Add conky artifact

---
+++ conky/README.md
@@ -0,0 +1,5 @@
+# Conky #
+Conky monitoring artifacts
+
+## Descriptions ##
+- `colorize.sh` A script to generate hex color codes for gradient colorization from GREEN to RED based on supplied `MIN` `MAX` and `sample` parametric values.
---
+++ conky/colorize.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+MAX=$1
+MIN=$2
+TEMPERATURE=$3
+
+if [ "$TEMPERATURE" -lt "$MIN" ] ; then
+	GRADIENT=0
+elif [ "$TEMPERATURE" -gt "$MAX" ] ; then
+	GRADIENT=100
+else
+	GRADIENT=$(bc<<<"scale=2;($TEMPERATURE - $MIN)*100/($MAX - $MIN)")
+fi
+
+if [ $(printf "%.0f" "$GRADIENT") -le 50 ] ; then
+	GREEN=255
+else
+	let GREEN=$(bc<<<"256-(($GRADIENT-50)*512)/100")
+fi
+
+if [ $(printf "%.0f" "$GRADIENT") -ge 50 ] ; then
+	RED=255
+else
+	let RED=$(bc<<<"($GRADIENT*512)/100")
+fi
+
+HEX=$(printf "%02x\n" $RED)$(printf "%02x\n" $GREEN)00
+echo "\${#${HEX}}$TEMPERATURE"
+
