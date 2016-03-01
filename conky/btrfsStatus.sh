446a030cc9a56decd80e068493c61107a19121ab
Mark Tompkins <mtompkins@users.noreply.github.com>
2016-03-01 06:31:44 -0500

    Update btrfsStatus.sh
    Improve scrub feedback.

--- conky/btrfsStatus.sh
+++ conky/btrfsStatus.sh
@@ -15,15 +15,15 @@ if [[ "$IS_BAL_CHK" == "running" ]] ; then
 	BAL_ARR_SIZE=${#IS_BAL_ARR[@]}
 	RETURN="BTRFS Balancing ${IS_BAL_ARR[$BAL_ARR_SIZE-2]}"
 elif [ "$IS_SRB_RUN" != "not running" ] ; then
-	echo SCRUBBING
 	BTR_USED=$($CHK_USED)
 	BTR_USED_ARR=($BTR_USED)
 	BTR_USED_AMT=${BTR_USED_ARR[3]}
 	BTR_USED_VAL=`sed 's/used=//;s/TiB$/*1024*1024*1024*1024/g;s/GiB$/*1024*1024*1024/g;s/MiB$/*1024*1024/g;s/KiB$/*1024/g' <<<$BTR_USED_AMT | bc -l`
 	BTR_SRB_VAL=`grep -oP '(?<=total bytes scrubbed: )[0-9.TGMKiB]+(?= with)' <<<$IS_SRB_ALL | sed 's/TiB$/*1024*1024*1024*1024/g;s/GiB$/*1024*1024*1024/g;s/MiB$/*1024*1024/g;s/KiB$/*1024/g' | bc -l`
 	BTR_PCT=100.0*$BTR_SRB_VAL/$BTR_USED_VAL
-	BTR_PCT_RTN=`bc -l <<< $BTR_PCT | awk '{printf("%d\n",$1 + 0.5)}'`
-	RETURN="BTRFS Scrubbing $BTR_PCT_RTN"
+	BTR_PCT_DONE=`bc -l <<< $BTR_PCT | awk '{printf("%d\n",$1 + 0.5)}'`
+	BTR_PCT_RTN=$((100 - $BTR_PCT_DONE))
+	RETURN="BTRFS Scrubbing: $BTR_PCT_RTN% remaining"
 else
 	RETURN=""
 fi
